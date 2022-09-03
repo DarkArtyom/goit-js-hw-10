328c264abaa4d49f5696b0945a67ca1c0f637e35
Artem Shnurko <shnurko.artyom@gmail.com>
2022-09-03 23:08:18 +0200

    task 90%

--- src/index.js
+++ src/index.js
@@ -1,16 +1,62 @@
 import './css/styles.css';
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+import API from './js/fetchCountries';
+
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
-
-var debounce = require('lodash.debounce');
+const countryListEl = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
 function onInput(e) {
-  const inputValue = e.target.value;
-  console.log(inputValue);
+  e.preventDefault();
+  const inputValue = e.target.value.trim();
+  console.log(API.fetchCountries(inputValue));
+  if (inputValue === '') {
+    return;
+  }
+  API.fetchCountries(inputValue).then(renderCountries).catch(onFetchError);
+  // .finnaly(() => inputEl.reset());
+}
+
+function renderCountries(inputValue) {
+  if (inputValue.length === 1) {
+    const countryTaken = inputValue.map(countryEl => {
+      const languageCountry = Object.values(countryEl.languages);
+      const countryDetaILS = `<div><img src=${countryEl.flags.svg} width="30" alt="flag" class="flag" /><h2>${countryEl.name.official}</h2></div><p>Capital: ${countryEl.capital}</p><p>Population: ${countryEl.population}</p><p>Languages: ${languageCountry}`;
+      return countryDetaILS;
+    });
+    countryInfo.innerHTML = countryTaken;
+    console.log('array 1');
+  } else {
+    const listOfChoosenCountries = inputValue.map(arrCountries => {
+      const renderListCountries = document.createElement('li');
+
+      const addFlagList = document.createElement('img');
+      addFlagList.src = `${arrCountries.flags.svg}`;
+      addFlagList.setAttribute = `alt: flag`;
+      addFlagList.setAttribute = `width=30`;
+
+      renderListCountries.append(addFlagList);
+
+      const addCountryName = document.createElement('p');
+      addCountryName.textContent = `${arrCountries.name.official}`;
+      renderListCountries.append(addCountryName);
+
+      return renderListCountries;
+    });
+
+    console.log(`array 2 and more`);
+    countryListEl.innerHTML = listOfChoosenCountries;
+  }
+}
+
+function onFetchError(error) {
+  console.log(`${error} Oops, there is no country with that name`);
 }
 
 // 1написать функцию fetchCountries(name) которая делает HTTP-запрос на ресурс name и возвращает промис с массивом стран - результатом
